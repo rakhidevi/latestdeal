c16383fb2714c99f9f201678ce5ebc9dc3434f1d
pankajtiwari86 <pankajtiwari86@gmail.com>
2026-07-20 15:31:22 +0530

    Fix PHP syntax error in BaseDealEvent

--- backend/app/Events/BaseDealEvent.php
+++ backend/app/Events/BaseDealEvent.php
@@ -8,29 +8,29 @@ use App\Models\Deal;
 abstract class BaseDealEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-    public \;
-    public \;
-    public \;
-    public \;
-    public \;
+    public $deal;
+    public $correlationId;
+    public $provider;
+    public $payloadVersion;
+    public $metadata;
 
-    public function __construct(Deal \, string \, string \ = 'unknown', string \ = '1.0', array \ = [])
+    public function __construct(Deal $deal, string $correlationId, string $provider = 'unknown', string $payloadVersion = '1.0', array $metadata = [])
     {
-        \->deal = \;
-        \->correlationId = \;
-        \->provider = \;
-        \->payloadVersion = \;
-        \->metadata = \;
-        \->logEvent();
+        $this->deal = $deal;
+        $this->correlationId = $correlationId;
+        $this->provider = $provider;
+        $this->payloadVersion = $payloadVersion;
+        $this->metadata = $metadata;
+        $this->logEvent();
     }
     
     protected function logEvent()
     {
-        \ = class_basename(\);
+        $event = class_basename($this);
         \App\Models\DealEvent::create([
-            'deal_id' => \->deal->id,
-            'event' => \,
-            'correlation_id' => \->correlationId,
+            'deal_id' => $this->deal->id,
+            'event' => $event,
+            'correlation_id' => $this->correlationId,
         ]);
     }
 }
