54a504ac23659bc41c972f2332ec70d190bb5897
pankajtiwari86 <pankajtiwari86@gmail.com>
2026-09-08 01:40:05 +0530

    Deploy index.php with debug catch

--- backend/public/index.php
+++ backend/public/index.php
@@ -17,4 +17,12 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+try {
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+    $kernel->terminate($request, $response);
+} catch (\Throwable $e) {
+    echo "<pre>FATAL EXCEPTION: " . $e->getMessage() . "\n" . $e->getTraceAsString() . "</pre>";
+    exit;
+}
